14e6a146a95c0b4a1ab0d41cd0c891a6232ca7a9
justbruno <ordozgoitipiedras@gmail.com>
2025-12-22 16:24:30 +0000

    Integral exponent. New simplex matrix.

--- src/lmbm/functions.py
+++ src/lmbm/functions.py
@@ -29,3 +29,16 @@ def sample_simplex_matrix(n: int = 2, ddf: int = 0) -> np.ndarray:
     S = np.random.random((n, n))
     S -= ddf * np.diag(np.diag(S))
     return S / np.sum(S, axis=0)
+
+
+def sample_simplex_matrix_concentrated(n: int = 2, ddf: int = 0, noise: float = 0) -> np.ndarray:
+    """
+    Generate a square random matrix whose columns are points in the simplex.
+    :param n: Number and dimension of the points.
+    :param ddf: Diagonal deflation factor.
+    :return: An ndarray containing the matrix.
+    """
+    S = np.eye(n) + np.random.random((n, n)) * noise
+    S = np.roll(S, shift=1, axis=1)
+    S -= ddf * np.diag(np.diag(S))
+    return S / np.sum(S, axis=0)
--- src/lmbm/generators/simplex.py
+++ src/lmbm/generators/simplex.py
@@ -4,11 +4,13 @@ import numpy as np
 from lmbm.functions import Softmax, sample_simplex_matrix, Lexicon
 from lmbm.encoding import SequenceToGamma
 
+
 class SimplexCombiner:
 
     def __init__(self, S: np.ndarray = None, lexicon: Lexicon = None,
                  context: int = 1000, beta: float = 1 / 2,
-                 prob_distribution_normalizer: Callable[[np.ndarray], np.ndarray] = None, seq2real = None):
+                 prob_distribution_normalizer: Callable[[np.ndarray], np.ndarray] = None,
+                 seq2real=None):
         n = lexicon.size
         if S is None:
             S = sample_simplex_matrix(n, ddf=.9)
@@ -23,42 +25,45 @@ class SimplexCombiner:
         self.eigenvalues, self.eigenvectors = np.linalg.eig(self.S)
         self.eigenvectors_inv = np.linalg.inv(self.eigenvectors)
 
+        self.entropy_estimate = EntropyEstimate()
+
         if seq2real is None:
-            self.seq2real = SequenceToGamma(alpha=1, lambda_=1)
+            enc = SequenceToGamma(alpha=1, lambda_=1)
+            self.seq2real = lambda x: 1 + enc(x)
+        else:
+            self.seq2real = seq2real
 
     def encode(self, seq: np.ndarray) -> np.ndarray:
         seq = seq[-self.context:]
         a = self.beta ** (np.arange(len(seq))[::-1])  # Larger beta for more recent tokens
 
-        # TODO Add eigenvalue decomposition to consider long range dependencies.
-        #  Note we need a deterministic mapping to a real for the exponent, adequately distributed (e.g. gamma + 2)
-        #  We want to see if 1.01 is still easy to learn
-        #  Heck, is 1 even easy to learn? Try an MLP or LSTM
-        # Core idea:
-        # v,V = np.linalg.eig(S) # This can go in constructor
-        # W = V.dot(np.diag(v**2.4)).dot(np.linalg.inv(V))
-        # return W[:, seq].dot(an)
-
         an = self.to_prob_distribution(a)
 
-        exponent = self.map_to_exponent(seq)
-        W = self.eigenvectors.dot(np.diag(self.eigenvalues**exponent)).dot(self.eigenvectors_inv[:, seq])
-        return W.dot(an)
-        # return self.S[:, seq].dot(an)
+        exponent = np.round(self.seq2real(seq))
 
-    def map_to_exponent(self, seq: np.ndarray) -> float:
-        """
-        Map sequence to real >= 2.
-        :param seq:
-        :return:
-        """
-        return 2.0 + self.seq2real(seq)
+        diagonal_items = np.power(self.eigenvalues, exponent, dtype=complex)
+        W = self.eigenvectors.dot(np.diag(diagonal_items)).dot(
+            self.eigenvectors_inv[:, seq])
+        return W.dot(an).real  # TODO Safe? Always real?
 
     def generate(self, n: int, input: np.ndarray = None) -> list:
         if input is None:
             input = np.random.choice(self.lexicon.lexicon, 1)
         for i in range(n):
             p = self.encode(input)
+            self.entropy_estimate.add_sample(p)
             token = np.random.choice(self.lexicon.lexicon, 1, p=p)
             input = np.hstack([input, token])
         return input
+
+
+class EntropyEstimate:
+    def __init__(self):
+        self.running_estimate = 0
+        self.n_samples = 0
+
+    def add_sample(self, p: np.ndarray[float]):
+        entropy = -p.dot(np.log(p))
+        self.running_estimate = ((self.n_samples * self.running_estimate + entropy)
+                                 / (self.n_samples + 1))
+        self.n_samples += 1
